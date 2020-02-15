ba9dc873ae4dd29e63b7635f2b71c785e363a92c
Bruce Gardner <bruce.gardner@sjsu.edu>
2020-02-15 15:59:42 -0800

    checks for key in dictory / plist file before attempting to find the date

--- password_expired.py
+++ password_expired.py
@@ -8,16 +8,17 @@ TODAY = dt.datetime.today()
 def check_password_expired():
    for user in os.listdir("/Users"):
       user_plist = os.path.join("/Users", user, "Library/Preferences/com.trusourcelabs.NoMAD.plist")
-      if os.path.exists(user_plist):
-         print(user_plist)
+      if os.path.isfile(user_plist):
+         print(user, user_plist)
 
          try:
             with open(user_plist, 'rb') as fp:
                pl = load(fp)
-            if pl['LastPasswordExpireDate'] > TODAY:
-               print('not expired')
-            else:
-               print('expired')
+            if 'LastPasswordExpireDate' in pl:
+               if pl['LastPasswordExpireDate'] > TODAY:
+                  print('not expired')
+               else:
+                  print('expired')
 
          except:
             print('unable to open file')
