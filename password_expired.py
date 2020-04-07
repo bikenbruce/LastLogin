aad680945135c1189763e1b48a4f8568f521eb91
Bruce Gardner <bikenbruce@gmail.com>
2020-04-06 20:11:49 -0700

    ran pycodestyle on it

--- password_expired.py
+++ password_expired.py
@@ -5,29 +5,38 @@ import dateutil.parser as dparser
 import os
 import subprocess
 
-TODAY = dt.datetime.today()
 
-def check_password_expired(delete_user = False):
-   for user in os.listdir("/Users"):
-      user_home_path = '/Users/' + user
-      user_plist = os.path.join(user_home_path, "Library/Preferences/com.trusourcelabs.NoMAD.plist")
-      if os.path.isfile(user_plist):
-         # print(user, user_plist)
+def check_password_expired(delete_user=False):
+    TODAY = dt.datetime.today()
 
-         try:
-            with open(user_plist, 'rb') as fp:
-               pl = load(fp)
-            if 'LastPasswordExpireDate' in pl:
-               if pl['LastPasswordExpireDate'] > TODAY:
-                  print('user: ' + user + '\texpire date: ' + str(pl['LastPasswordExpireDate'].date()) + '\tstate: not expired')
-               else:
-                  print('user: ' + user + '\texpire date: ' + str(pl['LastPasswordExpireDate'].date()) + '\tstate: expired')
-                  if delete_user:
-                      state = subprocess.call(['dscl', '.', '-delete', user_home_path], capture_output=True)
+    for user in os.listdir("/Users"):
+        user_home_path = '/Users/' + user
+        user_plist = os.path.join(
+            user_home_path,
+            "Library/Preferences/com.trusourcelabs.NoMAD.plist")
+        if os.path.isfile(user_plist):
+            # print(user, user_plist)
 
+            try:
+                with open(user_plist, 'rb') as fp:
+                    pl = load(fp)
+                if 'LastPasswordExpireDate' in pl:
+                    if pl['LastPasswordExpireDate'] > TODAY:
+                        print('user: ' + user + '\texpire date: ' +
+                              str(pl['LastPasswordExpireDate'].date()) +
+                              '\tstate: not expired')
+                    else:
+                        print('user: ' + user + '\texpire date: ' +
+                              str(pl['LastPasswordExpireDate'].date()) +
+                              '\tstate: expired')
+                        if delete_user:
+                            state = subprocess.call(['dscl', '.', '-delete',
+                                                    user_home_path],
+                                                    capture_output=True)
 
-         except IOError as err:
-            print(err)
+            except IOError as err:
+                print(err)
+
 
 if __name__ == "__main__":
-	check_password_expired()
+    check_password_expired()
