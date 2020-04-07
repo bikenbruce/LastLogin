c4b99aaa8a41b1bb967a489286584bb12f7e63bf
Bruce Gardner <bikenbruce@gmail.com>
2020-04-07 11:27:41 -0700

    changed running state to show output while archiving users home folder

--- common.py
+++ common.py
@@ -39,26 +39,7 @@ def ArchiveHomeFolder(user, destination_folder):
     destination = os.path.join(destination_folder, user)
     state = subprocess.run(['/usr/bin/hdiutil', 'create',
                             '-srcfolder', home_folder,
-                            '-format', 'ULFO', destination],
-                           capture_output=True)
-    if state.returncode == 0:
-        return(True)
-    else:
-        return(False)
-
-
-def ArchiveHomeFolderTest(user, destination_folder):
-    if not os.path.exists(destination_folder):
-        exit(1)
-    # Create DMG of home folder
-    print('archiving user folder:' + user + 'to:' + destination_folder)
-
-    home_folder = os.path.join('/Users', user)
-    destination = os.path.join(destination_folder, user)
-    state = subprocess.run(['/usr/bin/hdiutil', 'create',
-                            '-srcfolder', home_folder,
-                            '-format', 'ULFO', destination],
-                           capture_output=True)
+                            '-format', 'ULFO', destination])
     if state.returncode == 0:
         return(True)
     else:
