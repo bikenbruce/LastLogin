d8d62effd064e84c1bb097622a0a570e5ac7bc5a
Bruce Gardner <bikenbruce@gmail.com>
2020-04-08 02:50:50 -0700

    pretty heavy rewrite ofscript.  Added abilty to remove user record from AD if password has expired.

--- ad_password_expired.py
+++ ad_password_expired.py
@@ -1,39 +1,49 @@
 #!python
+from common import UserRecordExists, DeleteUserRecord
 from plistlib import *
 import datetime as dt
 import os
 
 
-def check_password_expired(delete_user=False):
-    TODAY = dt.datetime.today()
-
+def GetUsersPlist():
+    users = {}
     for user in os.listdir("/Users"):
         user_home_path = '/Users/' + user
         user_plist = os.path.join(
             user_home_path,
             "Library/Preferences/com.trusourcelabs.NoMAD.plist")
         if os.path.isfile(user_plist):
-            # print(user, user_plist)
+            users.update({user: user_plist})
+    return(users)
 
-            try:
-                with open(user_plist, 'rb') as fp:
-                    pl = load(fp)
-                if 'LastPasswordExpireDate' in pl:
-                    if pl['LastPasswordExpireDate'] > TODAY:
-                        print('user: ' + user + '\texpire date: ' +
-                              str(pl['LastPasswordExpireDate'].date()) +
-                              '\tstate: not expired')
-                    else:
-                        print('user: ' + user + '\texpire date: ' +
-                              str(pl['LastPasswordExpireDate'].date()) +
-                              '\tstate: expired')
-                        if delete_user:
-                            # remove_user_record(user, user_home_path)
-                            pass
 
-            except IOError as err:
-                print(err)
+def CheckUserState(user, user_plist):
+    today = dt.datetime.today()
+
+    with open(user_plist, 'rb') as fp:
+        nomad_plist = load(fp)
+        if 'LastPasswordExpireDate' in nomad_plist:
+            print('user:', user, '\texpire date:',
+                  str(nomad_plist['LastPasswordExpireDate'].date()), end='')
+
+            if nomad_plist['LastPasswordExpireDate'] > today:
+                print('\tstate: not expired')
+            else:
+                print('\tstate: expired')
+                RemoveUser(user)
+
+
+def RemoveUser(user):
+    if UserRecordExists(user):
+        if DeleteUserRecord(user):
+            print('\tRemoved user record from Directory Service')
+        else:
+            print('\tUnable to remove user record from Directory Service')
+    else:
+        print('\tUser record does not exist in Directory Service')
 
 
 if __name__ == "__main__":
-    check_password_expired()
+    users = GetUsersPlist()
+    for user, plist in users.items():
+        CheckUserState(user, plist)
