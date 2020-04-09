f49dd239d3436f82e29e325d07299d8e6fb6a9f3
Bruce Gardner <bikenbruce@gmail.com>
2020-04-08 19:53:35 -0700

    added black and ran it on the code

--- lastlogin.py
+++ lastlogin.py
@@ -16,7 +16,8 @@ import socket
 import subprocess
 
 THIRTY_DAYS = dt.timedelta(days=30)
-ADMINS = grp.getgrnam('admin').gr_mem
+ADMINS = grp.getgrnam("admin").gr_mem
+
 
 def check_local_users(days):
     """method to look at local users in /Users directory, and query the 'last'
@@ -28,25 +29,26 @@ def check_local_users(days):
         if os.path.isdir(os.path.join("/Users", user)):
             # print(user)
 
-            pipe = subprocess.run(
-                ['/usr/bin/last', '-1', user],
-                capture_output=True)
+            pipe = subprocess.run(["/usr/bin/last", "-1", user], capture_output=True)
 
             output = pipe.stdout.strip().decode("utf-8")
 
             # print(output)
 
-            if not output.strip().startswith('wtmp'):
+            if not output.strip().startswith("wtmp"):
                 # print(output.strip())
 
                 # if output contains login date / time range
                 # example: Fri Sep 27 14:03 - 14:03
-                if output.count(' - ') > 0:
-                    last_log = output.split(' - ')[0].split('   ')[-1:][0]
+                if output.count(" - ") > 0:
+                    last_log = output.split(" - ")[0].split("   ")[-1:][0]
                 else:
                     # print(output)
-                    last_log = output.replace('still logged in', '').\
-                               strip().split('   ')[-1:][0]
+                    last_log = (
+                        output.replace("still logged in", "")
+                        .strip()
+                        .split("   ")[-1:][0]
+                    )
                     # print(last_log)
 
                 try:
@@ -57,48 +59,62 @@ def check_local_users(days):
                     if last_log_time > today:
                         last_log_time = last_log_time - dt.timedelta(days=365)
 
-                    note = socket.gethostname().split('.')[0] + '-' +\
-                           last_log_time.strftime("%Y-%m-%d")
+                    note = (
+                        socket.gethostname().split(".")[0]
+                        + "-"
+                        + last_log_time.strftime("%Y-%m-%d")
+                    )
 
-                    print('user:', user, '\tlast_log_string:', last_log,
-                          '\tlast_log_datetime:',
-                          last_log_time.strftime("%a %b %_d, %Y %H:%M"))
+                    print(
+                        "user:",
+                        user,
+                        "\tlast_log_string:",
+                        last_log,
+                        "\tlast_log_datetime:",
+                        last_log_time.strftime("%a %b %_d, %Y %H:%M"),
+                    )
                     if today - last_log_time > days_ahead:
-                        print('\tgreater than 30 days')
-                        if user not in ADMINS: 
+                        print("\tgreater than 30 days")
+                        if user not in ADMINS:
                             ArchiveUser(user, note)
                         else:
-                            print('\t', user, 'is an admin, not archiving account')
+                            print("\t", user, "is an admin, not archiving account")
 
                 except ValueError:
-                    print(user, 'unable to extract date time')
+                    print(user, "unable to extract date time")
 
 
 def ArchiveUser(user, note=None):
     if UserRecordExists(user):
-        print('\tUser record exists in Directory Service')
+        print("\tUser record exists in Directory Service")
         if DeleteUserRecord(user):
-            print('\tRemoved user record from Directory Service')
+            print("\tRemoved user record from Directory Service")
     else:
-        print('\tUser record does not exist in Directory Service')
+        print("\tUser record does not exist in Directory Service")
 
     if UserHomeFolderExists(user):
-        print('\tUser home folder exists')
-        if ArchiveHomeFolder(user, '/Users', note):
-            print('\tUser home folder archived')
+        print("\tUser home folder exists")
+        if ArchiveHomeFolder(user, "/Users", note):
+            print("\tUser home folder archived")
             if DeleteHomeFolder(user):
-                print('\tUser home folder deleted')
+                print("\tUser home folder deleted")
         else:
-            print('\tAttempted to archive users home folder')
+            print("\tAttempted to archive users home folder")
     else:
-        print('\tUser home folder does not exist')
+        print("\tUser home folder does not exist")
 
 
 if __name__ == "__main__":
     PARSER = argparse.ArgumentParser(
-             description='show duration since last login for each user')
-    PARSER.add_argument('-d', '--days', type=int, default=30,
-                        help='number of days since last log in (defult: 30)')
+        description="show duration since last login for each user"
+    )
+    PARSER.add_argument(
+        "-d",
+        "--days",
+        type=int,
+        default=30,
+        help="number of days since last log in (defult: 30)",
+    )
     ARGS = PARSER.parse_args()
 
     check_local_users(ARGS.days)
--- passwordexpired.py
+++ passwordexpired.py
@@ -8,39 +8,44 @@ import os
 def GetUsersPlist():
     users = {}
     for user in os.listdir("/Users"):
-        user_home_path = '/Users/' + user
+        user_home_path = "/Users/" + user
         user_plist = os.path.join(
-            user_home_path,
-            "Library/Preferences/com.trusourcelabs.NoMAD.plist")
+            user_home_path, "Library/Preferences/com.trusourcelabs.NoMAD.plist"
+        )
         if os.path.isfile(user_plist):
             users.update({user: user_plist})
-    return(users)
+    return users
 
 
 def CheckUserState(user, user_plist):
     today = dt.datetime.today()
 
-    with open(user_plist, 'rb') as fp:
+    with open(user_plist, "rb") as fp:
         nomad_plist = load(fp)
-        if 'LastPasswordExpireDate' in nomad_plist:
-            print('user:', user, '\texpire date:',
-                  str(nomad_plist['LastPasswordExpireDate'].date()), end='')
+        if "LastPasswordExpireDate" in nomad_plist:
+            print(
+                "user:",
+                user,
+                "\texpire date:",
+                str(nomad_plist["LastPasswordExpireDate"].date()),
+                end="",
+            )
 
-            if nomad_plist['LastPasswordExpireDate'] > today:
-                print('\tstate: not expired')
+            if nomad_plist["LastPasswordExpireDate"] > today:
+                print("\tstate: not expired")
             else:
-                print('\tstate: expired')
+                print("\tstate: expired")
                 RemoveUserRecord(user)
 
 
 def RemoveUserRecord(user):
     if UserRecordExists(user):
         if DeleteUserRecord(user):
-            print('\tRemoved user record from Directory Service')
+            print("\tRemoved user record from Directory Service")
         else:
-            print('\tUnable to remove user record from Directory Service')
+            print("\tUnable to remove user record from Directory Service")
     else:
-        print('\tUser record does not exist in Directory Service')
+        print("\tUser record does not exist in Directory Service")
 
 
 if __name__ == "__main__":
